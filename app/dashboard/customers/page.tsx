b63d9ac7713e772a4638e0806a1d7032894da979
Avesh Khan <awesh155@gmail.com>
2024-09-03 12:58:25 +0530

    completed nextJs dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+
+export const metadata: Metadata = {
+    title: 'Customers',
+}
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+    const totalCustomers = await fetchFilteredCustomers(query)
+    return (
+        <>
+        <Suspense key={query} fallback={<TableRowSkeleton/>}>
+            <CustomersTable customers={totalCustomers} />
+        </Suspense>
+        </>
+    )
+
 }
